ae4efc1ba9a5c232d3f3369e3f51f19637d70d2e
dusixx <113414935+dusixx@users.noreply.github.com>
2023-05-01 12:11:10 +0300

    small fixes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 // components
 
-import { Task } from './Task/Task';
-import { UserProfile } from './UserProfile/UserProfile';
-import { Stats } from './Stats/Stats';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import { Task } from '../Task/Task';
+import { UserProfile } from '../UserProfile/UserProfile';
+import { Stats } from '../Stats/Stats';
+import { FriendList } from '../FriendList/FriendList';
+import { TransactionHistory } from '../TransactionHistory/TransactionHistory';
+import { Container } from './App.styled';
 
 // data
 
@@ -18,30 +19,26 @@ import transactions from 'data/transactions.json';
 // App
 
 export const App = () => (
-  <>
+  <Container>
     <Task
       title="01 - Social network profile"
       children={<UserProfile {...userData} />}
     />
-
     <Task
       title="02 - Upload stats"
       children={<Stats title="Upload stats" stats={uploadStats} />}
     />
-
     <Task
       title="02a - Upload stats (no title)"
       children={<Stats stats={uploadStats} />}
     />
-
     <Task
       title="03 - Friend list"
       children={<FriendList friends={friends} />}
     />
-
     <Task
       title="04 - Transaction history"
       children={<TransactionHistory items={transactions} />}
     />
-  </>
+  </Container>
 );
